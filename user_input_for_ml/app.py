e29ea2976547f71d6f79602a67cfe654f24c032e
kbrantner <brantner.kelsey@gmail.com>
2023-05-25 08:12:20 -0700

    first flask attempt

--- user_input_for_ml/app.py
+++ user_input_for_ml/app.py
@@ -1,5 +1,22 @@
 from flask import Flask, render_template
 from flask import jsonify
+from joblib import dump, load
+import pandas as pd
+import numpy as np
+#pd.set_option('max_colwidth', 400)
+import requests
+import matplotlib.pyplot as plt
+import json
+import re
+import warnings
+warnings.filterwarnings('ignore')
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+import tensorflow as tf
+
+#Im not sure if this should be with the open clause, I added that based on youtube video code
+rf_model=load('salary_ml_model.joblib')
+
 
 
 app = Flask(__name__)
@@ -7,7 +24,12 @@ app = Flask(__name__)
 
 @app.route('/')
 def webprint():
-    return render_template('inp.html') 
+    return render_template('input.html') 
+
+@app.route('/predict', methods=['POST', 'GET'])
+def resutls():
+
+    return render_template('input.html') 
 
 if __name__ == '__main__':
     app.run(debug=True)
